952b3596c2dc91d43f96c752979a68e9bc7ab19c
Lingcao Huang <huanglingcao@gmail.com>
2023-05-09 16:38:54 -0700

    download database

---
+++ download_database.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# download database of SAR imagery (ERS, ENVISat) and save to CSV
+python retrieve_catalog.py
+
+# convert to sqlite
+python to_sqlite.py
+
+# move to  ~/.local/share/asarapi
+db_dir=~/.local/share/asarapi
+mkdir -p ${db_dir}
+mv catalog.db  ${db_dir}
+
+
